1fa112dd48bb174d3e74686187078b8788d5e45e
Ivan <rivanv2008@gmail.com>
2026-03-24 12:11:03 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./style.css"
-import ThemeProvider from "./ThemeContext.jsx"
+import { ThemeProvider } from "./ThemeContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
